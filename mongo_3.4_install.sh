caadb6e96bbf40555c27f177677659b955c35794
Daniel Merron <DMerron593@sig.ads>
2017-01-09 14:35:55 +0000

    initial commit

---
+++ mongo_3.4_install.sh
@@ -0,0 +1,8 @@
+#install mongo 3.4
+#!/bin/bash
+
+sudo su
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+apt-get install -y mongodb-org
